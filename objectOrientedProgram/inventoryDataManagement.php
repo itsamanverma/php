650b235e2004fa49308e4b9e67ae2efd35b25509
itsamanverma <amanvermame786@gmail.com>
2019-02-07 19:44:20 +0530

    first program of object Oriented

---
+++ objectOrientedProgram/inventoryDataManagement.php
@@ -0,0 +1,15 @@
+ <?php
+/**
+* program to print prime anagrams using queue in 2d array
+* @author amanverma
+* @version 1.0
+*Date: 07/02/2019
+************************************************************/
+require('Utility.php');
+require('inventory.php');
+class Inventory
+{
+    public $name;
+    public $weight;
+    public $price;
+}
