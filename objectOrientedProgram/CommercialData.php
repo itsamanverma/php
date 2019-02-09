b3a9e32fec7b0bca9e801dbf62bbdb1d94042963
itsamanverma <amanvermame786@gmail.com>
2019-02-09 19:21:03 +0530

    Start the commericail program and using some concept of custom define error handler

---
+++ objectOrientedProgram/CommercialData.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* StockAccount.java implements a data type that
+* might be used by a financial institution to keep track of customer information.
+* the StockAccount class implements following methods  
+* @author amanverma
+* @version 1.0
+* Date: 08/02/2019
+******************/
+require('Utility.php');
+require_once('');
