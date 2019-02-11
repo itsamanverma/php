c8cc43976dd0700d1d502d6f88d00caecfd64b0e
itsamanverma <amanvermame786@gmail.com>
2019-02-11 14:44:16 +0530

    Proper Aligement

--- objectOrientedProgram/DeckOfCards.php
+++ objectOrientedProgram/DeckOfCards.php
@@ -5,61 +5,62 @@
  *@version 2.0
  *Date 08/01/2019
  */
-/* Top level error handler to handle errors */
-set_error_handler(function ($errNumber, $errMessage, $error_file, $error_line) {
+    require('Utility.php');
+    /* Top level error handler to handle errors */
+    set_error_handler(function ($errNumber, $errMessage, $error_file, $error_line) {
     echo "!!!!Error Occured!!!!!!!\n";
     echo "Error: [$errNumber] $errMessage - $error_file:$error_line \n";
     echo "Terminating!!!!!!!!!\n";
 
     die();
-});
+    });
 
-/**
- * Top level exception handler ;
- * called aitomaticaly by php at exception occurence
- */
-set_exception_handler(function ($e) 
-{
-    echo "\nException Occurred\n";
-    echo $e->getMessage();
-});
+   /**
+     *  Top level exception handler ;
+     * called aitomaticaly by php at exception occurence
+     */
+     set_exception_handler(function ($e) 
+     {
+     echo "\nException Occurred\n";
+     echo $e->getMessage();
+     });
 
-/* class to initialize the property of the card with suit and rank */
-class card
-{
-    /* variables to store properties od cars */
-    public $suit;
-    public $rank;
+     /* class to initialize the property of the card with suit and rank */
+     class card
+     {
+     /* variables to store properties od cars */
+     public $suit;
+     public $rank;
 
-    /* constructor of class */
-    function __construct($suit, $rank)
-    {
+     /* constructor of class */
+     function __construct($suit, $rank)
+     {
         $this->suit = $suit;
         $this->rank = $rank;
-    }
-}
+     }
+     }
 
-/**
- * finction to give the deck of cards as 2d array
- * @return arr the 2d array of the deck 
- */
-function getDeck()
-{
-    /* no of suits in the deck */
-    $suits = ["♣", "♦", "♥", "♠"];
-    /* no of ranks in the deck */
-    $rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    /* deck array  wth the empty value */
-    $deck = [];
-    for ($i = 0; $i < count($suits); $i++) {
+   /**
+     * finction to give the deck of cards as 2d array
+     * @return arr the 2d array of the deck 
+     */
+     function getDeck()
+     {
+     /* no of suits in the deck */
+     $suits = ["♣", "♦", "♥", "♠"];
+     /* no of ranks in the deck */
+     $rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+     /* deck array  wth the empty value */
+     $deck = [];
+     for ($i = 0; $i < count($suits); $i++) {
         for ($j = 0; $j < count($rank); $j++) {
             /* giving the values of cards in the deck array */  
             $deck[$i][$j] = new card($suits[$i], $rank[$j]);
-        }
-    }
+            }
+           }
     //print_r($deck);
-    return $deck;
-}
+     return $deck; 
+     }
 
 /**
  * shuffle the deck of cards and return it
@@ -84,51 +85,51 @@ function cardShuffle($deck)
         }
     }
 //    / print_r($deck);
-    return $deck;
-}
+    return $deck; 
+    }
 
-/**
- * distribute the deck of cards between 4 players   
- * @param deck the deck of cards 2d array (Shuffled)
- * @return players the 2d array of distributed cards
- */
-function distribute($deck)
-{
-    $players = [];
-    for ($i = 0; $i < 4; $i++) {
-        for ($j = 0; $j < 9; $j++) {
+      /**
+        * distribute the deck of cards between 4 players   
+        * @param deck the deck of cards 2d array (Shuffled)
+        * @return players the 2d array of distributed cards
+        */
+         function distribute($deck)
+        {
+        $players = [];
+        for ($i = 0; $i < 4; $i++) {
+           for ($j = 0; $j < 9; $j++) {
             $r = rand(0, 3);
             $c = rand(0, count($deck[$r]) - 1);
             $players[$i][$j] = $deck[$r][$c];
             array_splice($deck[$r], $c, 1);
-        }
-    }
-    return $players;
-}
+           }
+         } 
+        return $players;
+       }
 
-/**
- * Sort the player distributed cards and return it
- * @param player the 2d array containing the distributed cards
- * @return player the sorted distributed cards
- */
-function sortPlayer($player)
-{
-    for ($k = 0; $k < 4; $k++) {
-        for ($i = 1; $i < count($player[$k]); $i++) {
-        // getting value for back element
-            $j = ($i - 1);
-        //saving it in temperary variable;
-            $temp = $player[$k][$i];
-            while ($j >= 0 && $player[$k][$j]->rank >= $temp->rank) {
+   /**
+     * Sort the player distributed cards and return it
+     * @param player the 2d array containing the distributed cards
+     * @return player the sorted distributed cards
+     */
+      function sortPlayer($player)
+      {
+      for ($k = 0; $k < 4; $k++) {
+          for ($i = 1; $i < count($player[$k]); $i++) {
+           // getting value for back element
+               $j = ($i - 1);
+           //saving it in temperary variable;
+             $temp = $player[$k][$i];
+             while ($j >= 0 && $player[$k][$j]->rank >= $temp->rank) {
                 $player[$k][$j + 1] = $player[$k][$j];
                 $j--;
-            }
-            $player[$k][$j + 1] = $temp;
+              }
+              $player[$k][$j + 1] = $temp;
 
+          }
         }
-    }
-    return $player;
-}
+       return $player;
+      }
 
 /**
  * function to display the 2d array of distributed cards b/w 4 players
--- objectOrientedProgram/Utility.php
+++ objectOrientedProgram/Utility.php
@@ -1,4 +1,5 @@
 <?php
+
 /**********
  * Helper function containing methods to use in other php classes
  *@authour amanverma
@@ -20,7 +21,7 @@ class Utility
         }
         return $val;
     }
-     /**
+    /**
      * to get input unless its an String
      * @return String value
      */
@@ -65,9 +66,8 @@ class Utility
      */
     public static function String_input()
     {
-        fscanf(STDIN ,"%s\n", $str);
-        while(!(strlen($str)>0) || (is_numeric($str)) || $str==null)
-        {
+        fscanf(STDIN, "%s\n", $str);
+        while (!(strlen($str) > 0) || (is_numeric($str)) || $str == null) {
             echo "Enter a valid String \n";
             $str = Utility::String_input();
         }
@@ -78,11 +78,10 @@ class Utility
      */
     public static function getInteger()
     {
-        fscanf(STDIN ,"%s", $val);
-        while(!is_numeric($val) ||$val > (int)$val)
-        {
-          echo("enter the valin integer\n");
-          fscanf(STDIN ,"%s",$val);
+        fscanf(STDIN, "%s", $val);
+        while (!is_numeric($val) || $val > (int)$val) {
+            echo ("enter the valin integer\n");
+            fscanf(STDIN, "%s", $val);
         }
         return $val;
     }
@@ -90,43 +89,93 @@ class Utility
     /**
      * function to stored the normal combination
      */
-    public static function storeCards($cards,$rank,$suits){
-    
-        for($i=0;$i<sizeof($suits);$i++){
-            for($j=0;$j<sizeof($rank);$j++){
-                $cards[$i][$j] =$rank[$j].",".$suits[$i];
+    public static function storeCards($cards, $rank, $suits)
+    {
+
+        for ($i = 0; $i < sizeof($suits); $i++) {
+            for ($j = 0; $j < sizeof($rank); $j++) {
+                $cards[$i][$j] = $rank[$j] . "," . $suits[$i];
             }
         }
         return $cards;
     }
-     /**
-      * function to suffled the card 
-      */
-    public static function suffleCards($cardArr,$suits,$rank){
-        for($i=0;$i<sizeof($cards);$i++){
-            for($j=0;$j<sizeof($cards[$i]);$j++){
-                $rand1 = rand(0,3);
-                $rand2 = rand(0,11);
-               
+    /**
+     * function to suffled the card 
+     */
+    public static function suffleCards($cardArr, $suits, $rank)
+    {
+        for ($i = 0; $i < sizeof($cards); $i++) {
+            for ($j = 0; $j < sizeof($cards[$i]); $j++) {
+                $rand1 = rand(0, 3);
+                $rand2 = rand(0, 11);
+
                 $temp = $cards[$rand1][$rand2];
-                $cards[$rand1][$rand2]= $cards[$i][$j];
-                $cards[$i][$j]= $temp;
+                $cards[$rand1][$rand2] = $cards[$i][$j];
+                $cards[$i][$j] = $temp;
             }
         }
         return $cards;
     }
-     /**
-      * create the function to print the suffled Cards
-      */
-    public static function printCards($cards){
+    /**
+     * create the function to print the suffled Cards
+     */
+    public static function printCards($cards)
+    {
         echo "after suffling \n";
-        
-        for($i=0;$i<sizeof($cards);$i++){
-            echo "Player ".($i+1)." : [";
-            for($j=0;$j<sizeof($cards[$i]);$j++){
-                echo $cards[$i][$j]." ";
+
+        for ($i = 0; $i < sizeof($cards); $i++) {
+            echo "Player " . ($i + 1) . " : [";
+            for ($j = 0; $j < sizeof($cards[$i]); $j++) {
+                echo $cards[$i][$j] . " ";
             }
             echo "]\n\n";
         }
     }
+    /**
+     * finction to give the deck of cards as 2d array
+     * @return arr the 2d array of the deck 
+     */
+    public static function getDeck()
+    {
+    /* no of suits in the deck */
+        $suits = ["♣", "♦", "♥", "♠"];
+    /* no of ranks in the deck */
+        $rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    /* deck array  wth the empty value */
+        $deck = [];
+        for ($i = 0; $i < count($suits); $i++) {
+            for ($j = 0; $j < count($rank); $j++) {
+            /* giving the values of cards in the deck array */
+                $deck[$i][$j] = new card($suits[$i], $rank[$j]);
+            }
+        }
+    //print_r($deck);
+        return $deck;
+    }
+    /**
+     * shuffle the deck of cards and return it
+     * @param deck the 2d array containing deck of cards
+     * @return deck the shuffled deck of cards
+     */
+    public static function cardSuffled($deck)
+    {
+        for ($i = 0; $i < count($deck); $i++) {
+            for ($j = 0; $j < count($deck[$i]); $j++) {
+                $r1 = rand(0, 3);
+                $c1 = rand(0, 12);
+                $r = rand(0, count($deck));
+                $r2 = rand(0, 3);
+                $r = rand(0, count($deck));
+                $c2 = rand(0, 12);
+                $r = rand(0, count($deck));
+                $temp = $deck[$r1][$c1];
+                $r = rand(0, count($deck));
+                $deck[$r1][$c1] = $deck[$r2][$c2];
+                $deck[$r2][$c2] = $temp;
+            }
+        }
+//    / print_r($deck);
+        return $deck;
+    }
+
 }
